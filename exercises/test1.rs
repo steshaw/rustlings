b726001fb3a093d80ca7db2450cc38852f6f71f0
Steven Shaw <steven@steshaw.org>
2019-05-07 19:22:18 +1000

    Complete some exercises

--- exercises/test1.rs
+++ exercises/test1.rs
@@ -8,7 +8,13 @@
 // the price of an order of apples given the order amount. No hints this time!
 
 // Put your function here!
-// fn ..... {
+fn calculateprice(num : i32) -> i32 {
+    if num > 40 {
+        num
+    } else {
+        num * 2
+    }
+}
 
 // Don't modify this function!
 #[test]
--- exercises/tests/tests3.rs
+++ exercises/tests/tests3.rs
@@ -3,8 +3,8 @@
 // the test passes. Then write a second test that tests whether we get the result
 // we expect to get when we call `is_even(5)`. Scroll down for hints!
 
-pub fn is_even(num: i32) -> bool {
-    num % 2 == 0
+pub fn is_even(i: i32) -> bool {
+    i % 2 == 0
 }
 
 #[cfg(test)]
@@ -13,7 +13,12 @@ mod tests {
 
     #[test]
     fn is_true_when_even() {
-        assert!();
+        assert!(is_even(12));
+    }
+
+    #[test]
+    fn is_false_when_not_even() {
+        assert_eq!(false, is_even(5));
     }
 }
 

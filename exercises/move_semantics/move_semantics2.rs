aa89a2e55398b436c3d547187e9fbd50cc07a264
Steven Shaw <steven@steshaw.org>
2019-12-29 20:18:21 +1000

    move_semantics2c

--- exercises/move_semantics/move_semantics2.rs
+++ exercises/move_semantics/move_semantics2.rs
@@ -2,8 +2,8 @@
 // Make me compile without changing line 10! Scroll down for hints :)
 
 fn main() {
-    let vec0 = Vec::new();
-    let mut vec1 = fill_vec(&vec0);
+    let mut vec0 = Vec::new();
+    let mut vec1 = fill_vec(&mut vec0);
 
     // Do not change the following line!
     println!("{} has length {} content `{:?}`", "vec0", vec0.len(), vec0);
@@ -14,14 +14,12 @@ fn main() {
 
 }
 
-fn fill_vec(vec: &Vec<i32>) -> Vec<i32> {
-    let mut vec = vec.to_vec();
-
+fn fill_vec(vec: &mut Vec<i32>) -> Vec<i32> {
     vec.push(22);
     vec.push(44);
     vec.push(66);
 
-    vec
+    vec.to_vec()
 }
 
 

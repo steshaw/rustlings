1a69ea8b842d5b965c625d2a6755e2e35d4f7695
Steven Shaw <steven@steshaw.org>
2019-12-30 13:23:25 +1000

    Use ? operator

--- exercises/error_handling/errors2.rs
+++ exercises/error_handling/errors2.rs
@@ -21,8 +21,8 @@ use std::num::ParseIntError;
 pub fn total_cost(item_quantity: &str) -> Result<i32, ParseIntError> {
     let processing_fee = 1;
     let cost_per_item = 5;
-    let qty = item_quantity.parse::<i32>();
-    qty.map(|qty| qty * cost_per_item + processing_fee)
+    let qty = item_quantity.parse::<i32>()?;
+    Ok(qty * cost_per_item + processing_fee)
 }
 
 #[cfg(test)]

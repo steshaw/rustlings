d9de26055894cf2aaa0752a3b3425277ccbf86b3
Steven Shaw <steven@steshaw.org>
2020-01-02 10:14:50 +1000

    exercises

--- exercises/primitive_types/primitive_types4.rs
+++ exercises/primitive_types/primitive_types4.rs
@@ -8,7 +8,11 @@
 fn slice_out_of_array() {
     let a = [1, 2, 3, 4, 5];
 
-    let nice_slice = ???
+    for i in 0..a.len() {
+        println!("a[{}] = {}", i, a[i]);
+    }
+
+    let nice_slice = &a[1..=3];
 
     assert_eq!([2, 3, 4], nice_slice)
 }
--- exercises/test1.rs
+++ exercises/test1.rs
@@ -7,17 +7,22 @@
 // more than 40 at once, each apple only costs 1! Write a function that calculates
 // the price of an order of apples given the order amount. No hints this time!
 
-// I AM NOT DONE
-
 // Put your function here!
-// fn ..... {
+fn calculate_apple_price(quantity: i32) -> i32 {
+    let price_per_item = if (quantity > 40) { 1 } else { 2 };
+    price_per_item * quantity
+}
 
 // Don't modify this function!
 #[test]
 fn verify_test() {
-    let price1 = calculate_apple_price(35);
-    let price2 = calculate_apple_price(65);
+    assert_eq!(0, calculate_apple_price(0));
+    assert_eq!(2, calculate_apple_price(1));
+    assert_eq!(4, calculate_apple_price(2));
 
+    let price1 = calculate_apple_price(35);
     assert_eq!(70, price1);
+
+    let price2 = calculate_apple_price(65);
     assert_eq!(65, price2);
 }
